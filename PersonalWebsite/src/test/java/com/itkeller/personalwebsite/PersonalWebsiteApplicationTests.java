86fccf7bf268be7a8190feb823dcc30baaf7b08c
7uuki <lukaskeller348@gmail.com>
2025-02-16 17:33:22 +0100

    spring boot init

---
+++ PersonalWebsite/src/test/java/com/itkeller/personalwebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkeller.personalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
